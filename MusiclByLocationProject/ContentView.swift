5194336fa7420f21dfaa3821ba151c773751e75c
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-03-02 18:40:23 +0000

    Initial Commit

---
+++ MusiclByLocationProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusiclByLocationProject
+//
+//  Created by Shanahan, Krystian (JD) on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusiclByLocationProject/MusiclByLocationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiclByLocationProjectApp.swift
+//  MusiclByLocationProject
+//
+//  Created by Shanahan, Krystian (JD) on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusiclByLocationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
